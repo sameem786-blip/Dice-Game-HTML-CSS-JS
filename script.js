340218ecfead35f769f1c561012e9f7f76f73280
Sameem Abbas <sameemabbas2002@outlook.com>
2021-03-24 22:35:43 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+console.log(randomNumber1);
+console.log(randomNumber2);
+
+document.querySelector('.img1').src = "images/dice" + randomNumber1 + ".png";
+document.querySelector('.img2').src = "images/dice" + randomNumber2 + ".png";
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML = 'Player1 Wins';
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector('h1').innerHTML = 'Player2 Wins';
+} else {
+    document.querySelector('h1').innerHTML = 'Draw';
+}
